ef840409030234bece8a4b6057ece3bb32916aec
Elliot Cheung <elliotccheung@gmail.com>
2017-02-22 15:03:55 +0000

    Did some cleanup; removed a console.log

--- components/app.js
+++ components/app.js
@@ -13,14 +13,13 @@ App.controller = function(){
   ctrl.generate = function() {
     return m.request({method: "GET", url: "/randomhgt"})
     .then((resp) => {
-      console.log("resp: ", resp);
       ctrl.hgt = resp.data.split(' ').map(s => s.charAt(0).toUpperCase() + s.slice(1)).join(' ');
     });
   }
 }
 
 App.view = function(ctrl){
-  return m('gibbins', {class:'default'}, [
+  return m('div', {class:'default'}, [
       m('h1', ctrl.hgt),
       m('div'),
       m('button', {onclick: ctrl.generate}, "Generate"),
